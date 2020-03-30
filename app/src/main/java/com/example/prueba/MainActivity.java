a2cc7dcd1a4ce8d0b86dae45202e3692c3ab0640
minimafy <62904920+minimafy@users.noreply.github.com>
2020-03-30 21:36:45 +0200

    prueba

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         viewpager.setAdapter(onboardingAdapter);
         setupOnBoardingIndicator();
         setCurrentOnBoardingIndicator(0);
-        String fianal="";
+        String fianal="github";
 
 
         viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {

012030e50b3ef206fd9c25e84c77004b59f4807c
minimafy <62904920+minimafy@users.noreply.github.com>
2020-05-24 18:57:48 +0200

    commit

--- app/src/main/java/com/example/prueba/MiPerfil.java
+++ app/src/main/java/com/example/prueba/MiPerfil.java
@@ -1,5 +1,6 @@
 package com.example.prueba;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,6 +15,16 @@ import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.prueba.Objetos.Usuario;
+import com.example.prueba.Objetos.Usuario2;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -41,6 +52,12 @@ public class MiPerfil extends AppCompatActivity {
     private EditText correo;
     private EditText password;
     private Dialog qr_dialog;
+
+    private FirebaseDatabase base;
+    private DatabaseReference referencia;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private Usuario2 usuario2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +95,12 @@ public class MiPerfil extends AppCompatActivity {
                 finish();
             }
         });
+
+        base=FirebaseDatabase.getInstance();
+        referencia=base.getReference("Usuarios");
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
+
 
 
     btn_qr.setOnClickListener(new View.OnClickListener() {
@@ -121,6 +144,7 @@ public class MiPerfil extends AppCompatActivity {
                     btn_modificar.setText("Confirmar cambios");
 
 
+
                     telefono.setEnabled(true);
                     funcion_mod="conf";
                 }
@@ -147,6 +171,8 @@ public class MiPerfil extends AppCompatActivity {
                       }
                     }
                     if(salida==true){
+                        usuario2.setTelefono(telefono.getText().toString());
+                        referencia.child(usuario2.getReferencia()).setValue(usuario2);
                         btn_contraseña.setVisibility(View.VISIBLE);
                         btn_cerrar_sesion.setVisibility(View.VISIBLE);
                         btn_modificar.setBackgroundColor(Color.rgb(33,150,244));
@@ -164,7 +190,28 @@ public class MiPerfil extends AppCompatActivity {
 
             public boolean validCellPhone(String number) { return android.util.Patterns.PHONE.matcher(number).matches(); }
         });
+        leerDatos();
+    }
+    public void leerDatos(){
+        referencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Usuario usuario = data.getValue(Usuario.class);
+                    if (usuario.getId().equals(user.getUid())) {
+                        usuario2 = new Usuario2(data.getKey(), usuario.getId(), usuario.getNombre_usuario(), usuario.getEmail(), usuario.getTelefono(), usuario.getUrl_imagen());
+                    }
+                }
+                username_superior.setText(usuario2.getNombre_usuario());
+                username.setText(usuario2.getNombre_usuario());
+                telefono.setText(usuario2.getTelefono());
+                correo.setText(usuario2.getEmail());
 
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
     }
 }
